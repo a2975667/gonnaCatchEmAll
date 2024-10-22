7f66881751bf0b9a6f8b3fe471235e6e714bf64c
Ti-Chung Cheng <tcheng10@illinois.edu>
2024-10-22 00:03:01 -0500

    section-5

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,12 @@ import express, { Request, Response } from 'express';
 import pokemonRoutes from './src/routes/pokemonRoutes';
 import pokemonSpawnRoutes from './src/routes/pokemonSpawnRoutes';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3007;
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +18,10 @@ app.get('/api/', (req:Request, res:Response) => {
 
 app.use("/api/pokemon", pokemonRoutes);
 app.use("/api/pokemon-spawns", pokemonSpawnRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Pokedex is running on http://localhost:${PORT}`);
